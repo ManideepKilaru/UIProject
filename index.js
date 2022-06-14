1b14d70a93055cc99f129547aa0efd8d9fd2be57
Girija Rani Nimmagadda <gnimmaga@asu.edu>
2022-06-13 20:53:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv")
+dotenv.config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
+
+mongoose.connect(process.env.dbURL)
+    .then(console.log("DB Connected"))
+    .catch(error => console.log(error))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Header", "Origin,X-Requested-Width,Content-Type,Accept,Authorization")
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    next();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
